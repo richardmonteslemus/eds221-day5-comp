f5623b5f1a1e0d405ec20e4cc91408c2bea5a0ff
richardmonteslemus <richard.montes.lemus@gmail.com>
2025-08-15 11:29:52 -0700

    sourcing functions

---
+++ src/food_cart_function.R
@@ -0,0 +1,8 @@
+name_cart <- function(food, animal) {
+  print(paste0("Mc", stringr::str_to_title(animal), "'s ",
+               stringr::str_to_title(food), " Mart"))
+}
+
+# Example 
+#name_cart(food = "burrito", animal = "pika")
+
